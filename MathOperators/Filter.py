eccb0b13434436ac0615a3fce6c9de71cf77e0ba
Ryrybros <ryadgourine1@gmail.com>
2025-11-20 00:41:54 +0100

    setting up filters

---
+++ MathOperators/Filter.py
@@ -0,0 +1,15 @@
+
+from filetools.jsonHandler import jsonHandler
+import numpy as np
+
+
+def FIR():
+    
+    data = jsonHandler.readJson("data/Glasberg2002.json")    
+    tfLinear = 10.^(data["OuterMiddleEar1997"]["tfOuterMiddle"]/10)
+    print(tfLinear)
+    
+
+FIR()
+
+
